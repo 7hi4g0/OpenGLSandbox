a26c9da0d46542d874c1f13afb788c7efacc5fd8
Thiago Dourado de Andrade <7hi4g0@gmail.com>
2015-09-23 14:32:16 -0400

    Subdivide creates a new model

--- Subdivision/subd.cpp
+++ Subdivision/subd.cpp
@@ -3,6 +3,7 @@
 void Subdivide(Model *model) {
 	Model newModel;
 	std::set<Face>::iterator it;
+	int posIndex = 0;
 
 	for (it = model->faces.begin(); it != model->faces.end(); it++) {
 		Face face = *it;
@@ -15,33 +16,49 @@ void Subdivide(Model *model) {
 		}
 		facePos = facePos / face.numVertices;
 
+		if (facePos.x == 0 && facePos.y == 0 && facePos.z == 0) {
+			cout << *face.pos[0] << " " << *face.pos[1] << " " << *face.pos[2] << " " << *face.pos[3] << " " << endl;
+		}
+
 		for (int vertice = 0; vertice < face.numVertices; vertice++){
 			Face subface;
 			Edge from;
 			Edge to;
 			Position fromPos;
 			Position toPos;
+			Position normal;
 
 			subface.numVertices = 4;
 			from = *face.edges[vertice];
 			to = *face.edges[(vertice - 1) % face.numVertices];
 
 			//Vertice 1
-			subface.pos[0] = face.pos[vertice];
+			newModel.pos.push_back(*face.pos[vertice]);
+			subface.pos[0] = &newModel.pos.back();
 
 			//Vertice 2
 			fromPos = (*from.v0 + *from.v1) / 2;
-			model->pos.push_back(fromPos);
-			subface.pos[1] = &model->pos.back();
+			newModel.pos.push_back(fromPos);
+			subface.pos[1] = &newModel.pos.back();
+
+			if (fromPos.x == 0 && fromPos.y == 0 && fromPos.z == 0) {
+				cout << *from.v0 << " " << *from.v1 << " " << endl;
+			}
 
 			//Vertice 3
-			model->pos.push_back(facePos);
-			subface.pos[2] = &model->pos.back();
+			newModel.pos.push_back(facePos);
+			subface.pos[2] = &newModel.pos.back();
 
 			//Vertice 4
 			toPos = (*to.v0 + *to.v1) / 2;
-			model->pos.push_back(toPos);
-			subface.pos[3] = &model->pos.back();
+			newModel.pos.push_back(toPos);
+			subface.pos[3] = &newModel.pos.back();
+
+			if (toPos.x == 0 && toPos.y == 0 && toPos.z == 0) {
+				cout << *to.v0 << " " << *to.v1 << " " << endl;
+			}
+
+			normal = *face.normals[vertice];
 
 			for (int i = 0; i < 4; i++) {
 				std::pair<std::set<Edge>::iterator, bool> ret;
@@ -57,6 +74,10 @@ void Subdivide(Model *model) {
 					ret = newModel.edges.insert(e);
 					it = ret.first;
 				}
+				newModel.normals.push_back(normal);
+				newModel.quadIndices.push_back(posIndex++);
+
+				subface.normals[i] = &newModel.normals.back();
 				subface.edges[i] = &*it;
 			}
 
